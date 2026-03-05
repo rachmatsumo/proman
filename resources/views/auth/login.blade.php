0c9230ca9ec2af499445265f52a97fd965ed08b9
Abdul Rachmat <idsumoblog@gmail.com>
2026-03-05 14:03:24 +0000

    Improve

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - ProMan</title>
+    <meta property="og:description" content="Solusi manajemen proyek terintegrasi untuk tim Anda. Kelola program, proyek, dan agenda dengan mudah di ProMan.">
+    <meta property="og:image" content="{{ asset('img/thumbnail.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/proman-fav.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
